650e0ab9d61510d2596e5cc18211698e3accc421
lea78 <lea.hamzi@edu.itescia.fr>
2021-01-28 09:54:29 +0100

    css caroussel

--- views/login_view.php
+++ views/login_view.php
@@ -1,22 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>Login • Nail</title>
-    <link rel="stylesheet" href="../design/loginstyle.css">
-    <link rel="shortcut icon" type="image/x-icon" href="instagram-logo.ico.png" sizes="16x16">
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../design/style.css">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <!-- script for the log in page -->
-    <script src="./loginscript.js"></script>
-        <!-- bootstrap -->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-        <!-- carousel -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js"></script>
+    
 </head>
 <body>
     <section class="content">
@@ -25,61 +14,20 @@
             <div class="password_username">
                 <input type="text" placeholder="Username" required="" class="username"/>
                 <input type="password" placeholder="Password" required="" class="password"/>
-            </form>
                 <a href="#" class="lnkforget">Forgot?</a>
-                <input type="submit" value="Log in" class="submit"/>
+                <input type="submit" value="Log in"  class="submit"/>
             </div>
-                </section>
+        </form>           
+    </section>
         <div class = "no_account">
-                <h4>Don't have an account?<a href="./signup_view.php"> Sign up here</a></h4>
-            </div>
-    
-        <!-- Google font -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Titillium+Web:400,400i,600,600i,700,700i,900">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i,800,800i">
-        <!-- Bootstrap -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-        <!-- icofont -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionic/1.3.2/css/ionic.css">
-        <!-- carousel -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
-
-<section class="blog_section">
-            <div class="container">
-                <div class="blog_content">
-                    <div class="owl-carousel owl-theme">
-                        <div class="blog_item">
-                            <div class="blog_image">
-                                <img class="img-fluid" src="https://cdn.pixabay.com/photo/2019/03/10/18/31/hong-kong-4046913_960_720.jpg" alt="images not found">
-                                
-                            </div>
-                            <div class="blog_detailsd">
-                                
-                                
-                            </div>
-                        </div>                        
-                        <div class="blog_item">
-                            <div class="blog_image">
-                                <img class="img-fluid" src="https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667_960_720.jpg" alt="images not found">
-                                <span><i class="icon ion-md-create"></i></span>
-                            </div>
-                            
-                        </div>                        
-                        <div class="blog_item">
-                            <div class="blog_image">
-                                <img class="img-fluid" src="https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_960_720.jpg" alt="images not found">
-                            </div>
-                                 
-                                
-                                <a href="#"><i class="icon font-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+            <h4>Don't have an account?<a href="./signup_view.php"> Sign up here</a></h4>
+        </div>
+    </section>
+    <section class= "slideshow-container">   
+            <img class ="mySlides fade" src="../design/image/nail-2021.jpg" style = "width:40%">
+            <img class ="mySlides fade" src="../design/image/nail-paillette.jpg" style = "width:40%">
+            <img class ="mySlides fade" src="../design/image/nail-freestyle.jpg" style = "width:40%">
+    </section>
 </body>
-<footer>
-</footer>
-
+<script src= "./script.js"></script>
+</html>
