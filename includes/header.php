7ba4de14d5e75552f426dc88dc7534de9842e569
alexsoulisweb <alexsoulisdev@outlook.com>
2021-02-04 16:15:44 +0100

    NavBar on the home page

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,17 @@
     <form class= "search" action="">
         <input  type="text-area" placeholder="search">
     </form>
-    <nav class = "nav_icone">
-        <a  href="./views/createPublication_view.php"><img src="https://img.icons8.com/pastel-glyph/64/000000/plus--v1.png" class = "icone"><span class="tootiptext">Create</span></a>
-        <a  href="./profil"><img src="./design/image/user.png" class = "icone"><span class="tootiptext">Profil</span> </a>        
-    </nav>
+
+    
+    <nav>
+    <div class="bottle-container">
+    <div class = "click-to-top">
+        <a  class="active tooltip-icon" href="./views/createPublication_view.php"><img src="https://img.icons8.com/pastel-glyph/64/000000/plus--v1.png" class = "icone"><span class="tooltiptext">Create</span></a></nav>
+    <div class="click-to-top">  
+        <a  class="active tooltip-icon" href="./profil"><img src="./design/image/user.png" class = "icone"><span class="tooltiptext" class="icone">Profil</span> </a> </div>  
+        </div>
+        </nav>
+    
 </header>
+
+
